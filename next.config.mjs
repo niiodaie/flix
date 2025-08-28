5eda7665e42bcf413f105aa356a1481ba70ec848
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-08-28 02:24:11 -0500

    Update and rename next.config.mjs to next.config.js

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
-  // Keep false unless using 'critters' plugin explicitly
-  experimental: { optimizeCss: false },
-  // Optional: silence React strict double-render in dev if needed
-  // reactStrictMode: true,
-};
-module.exports = nextConfig;
