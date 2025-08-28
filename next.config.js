2c2ea110435f68a08873a968b9294f05c0892208
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-08-28 01:32:29 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import(\'next\').NextConfig} */
-const nextConfig = {
-  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
-  experimental: { optimizeCss: true }
-};
-module.exports = nextConfig;
