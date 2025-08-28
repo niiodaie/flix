cab9568d9d9c8cb089d3a7af5973549031733d8e
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-08-28 01:32:42 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
+  experimental: { optimizeCss: true }, // requires 'critters' (step 2)
 };
 
 export default nextConfig;
